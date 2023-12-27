9ccca0f3986b3c760c9822642f8acd378c70ca6f
Jakob <jakob@winter-wind.de>
2023-12-27 10:56:20 +0100

    add file; add basic class structure

---
+++ src/data/helmholtz/meshing/DomainManager.py
@@ -0,0 +1,12 @@
+class MeshManager:
+    """manages domain descriptions and meshes
+
+    """
+
+    def __init__(self):
+        self.mesh = None
+        self.boundaries = None
+        self.mesh_strategy = None
+
+    def get_mesh(self):
+        pass
