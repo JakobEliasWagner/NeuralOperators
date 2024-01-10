11908e051b63573f614c71a412c3af858ec0042b
Jakob <jakob@winter-wind.de>
2024-01-10 12:01:25 +0100

    expose classes

--- src/data/helmholtz/domain_properties/__init__.py
+++ src/data/helmholtz/domain_properties/__init__.py
@@ -1,5 +1,5 @@
-from description import CShapeDescription, CylinderDescription, Description
-from file_input import read_config
+from .description import CShapeDescription, CylinderDescription, Description
+from .file_input import read_config
 
 __all__ = [
     "read_config",
