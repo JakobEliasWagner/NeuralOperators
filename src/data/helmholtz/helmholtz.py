03bd3bb3c2c20739d42093018864a1b6c5c1f897
Jakob Elias Wagner <jakob.e.wagner@tum.de>
2024-01-13 16:21:46 +0100

    rm evaluation; rm print statements

--- src/data/helmholtz/helmholtz.py
+++ src/data/helmholtz/helmholtz.py
@@ -21,6 +21,7 @@ class Helmholtz:
     """
 
     def __init__(self, problem_description_file: pathlib.Path, out_dir: pathlib.Path):
+        print(problem_description_file)
         self.descriptions = read_config(problem_description_file)
         self.out_dir = out_dir
 
