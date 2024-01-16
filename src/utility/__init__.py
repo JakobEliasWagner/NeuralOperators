034791dad697c99197284ea085d7af5e88f4fa83
Jakob Elias Wagner <jakob.e.wagner@tum.de>
2024-01-16 20:53:24 +0100

    expose progress monitor

--- src/utility/__init__.py
+++ src/utility/__init__.py
@@ -1,5 +1,11 @@
 from .decorators import run_once
+from .progress_monitor import ProgressMonitor
 from .unique_id import get_unique_id
 from .xml_dict import XmlDict
 
-__all__ = ["run_once", "get_unique_id", "XmlDict"]
+__all__ = [
+    "run_once",
+    "get_unique_id",
+    "XmlDict",
+    "ProgressMonitor",
+]
