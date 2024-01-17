4a4456d230409e972549cad971786367972f9b15
Jakob <jakob@winter-wind.de>
2024-01-17 08:51:19 +0100

    rm deprecated wrapper

--- src/utility/__init__.py
+++ src/utility/__init__.py
@@ -1,9 +1,7 @@
-from .decorators import run_once
 from .progress_monitor import ProgressMonitor
 from .unique_id import get_unique_id
 
 __all__ = [
-    "run_once",
     "get_unique_id",
     "ProgressMonitor",
 ]
