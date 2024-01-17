e68ee2ae60f499d00073ac3230acf2f9640ab4fe
Jakob <jakob@winter-wind.de>
2024-01-17 08:44:16 +0100

    rm deprecated xml dict

--- src/utility/__init__.py
+++ src/utility/__init__.py
@@ -1,11 +1,9 @@
 from .decorators import run_once
 from .progress_monitor import ProgressMonitor
 from .unique_id import get_unique_id
-from .xml_dict import XmlDict
 
 __all__ = [
     "run_once",
     "get_unique_id",
-    "XmlDict",
     "ProgressMonitor",
 ]
