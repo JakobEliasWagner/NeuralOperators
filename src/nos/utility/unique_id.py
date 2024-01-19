e8dfa470533e5b82222ccc7b9e26d2b80d3c8246
Jakob Elias Wagner <jakob.e.wagner@tum.de>
2024-01-19 12:01:33 +0100

    fix thread safety

--- src/nos/utility/unique_id.py
+++ src/nos/utility/unique_id.py
@@ -1,6 +1,7 @@
 import random
+import warnings
 from datetime import datetime
-from uuid import UUID
+from uuid import UUID, uuid4
 
 
 class UniqueId:
@@ -15,9 +16,13 @@ class UniqueId:
         self.time_stamp = time_stamp
 
         # uuid part
-        rng = random.Random()
-        rng.seed(seed)
-        self.uuid = UUID(int=rng.getrandbits(128), version=4)
+        if seed is not None:
+            warnings.warn("Setting the seed for generating unique ID may compromise thread safety!")
+            rng = random.Random()
+            rng.seed(seed)  # defaults to system time when None -> thread safety compromised
+            self.uuid = UUID(int=rng.getrandbits(128), version=4)
+        else:
+            self.uuid = uuid4()
 
     def __str__(self):
         return self.time_stamp.strftime("%Y%m%d%H%M%S-") + str(self.uuid)
