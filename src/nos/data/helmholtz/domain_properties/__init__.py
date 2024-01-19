c56e4ecf1a291190fb1f861acc028f529e9baa46
Jakob Elias Wagner <jakob.e.wagner@tum.de>
2024-01-19 11:25:44 +0100

    update

--- src/nos/data/helmholtz/domain_properties/__init__.py
+++ src/nos/data/helmholtz/domain_properties/__init__.py
@@ -1,7 +1,9 @@
 from .crystal_description import CShapeDescription, CylinderDescription, NoneDescription
+from .domain_description import Description
 
 __all__ = [
     "CylinderDescription",
     "CShapeDescription",
     "NoneDescription",
+    "Description",
 ]
