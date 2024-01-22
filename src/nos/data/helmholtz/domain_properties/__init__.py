8ab8c530e18e75682e369c5ae92cebb94e57da87
Jakob Elias Wagner <jakob.e.wagner@tum.de>
2024-01-22 10:01:00 +0100

    change outer radius to radius to abuse similarity

--- src/nos/data/helmholtz/domain_properties/__init__.py
+++ src/nos/data/helmholtz/domain_properties/__init__.py
@@ -1,8 +1,9 @@
-from .crystal_description import CShapeDescription, CylinderDescription, NoneDescription
+from .crystal_description import CrystalDescription, CShapeDescription, CylinderDescription, NoneDescription
 from .domain_description import Description
 from .file_input import read_config
 
 __all__ = [
+    "CrystalDescription",
     "CylinderDescription",
     "CShapeDescription",
     "NoneDescription",
