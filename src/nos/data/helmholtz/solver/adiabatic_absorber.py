bec3c3bd45a718299fcb79707ad18d5bfda9ecf1
Jakob Elias Wagner <jakob.e.wagner@tum.de>
2024-01-23 17:30:31 +0100

    add adiabatic absorber class

---
+++ src/nos/data/helmholtz/solver/adiabatic_absorber.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+from nos.data.helmholtz.domain_properties import Description
+from nos.utility import BoundingBox2D
+
+from .wave_number_function import WaveNumberFunction
+
+
+class AdiabaticAbsorber(WaveNumberFunction):
+    """Adiabatic layer class to truncate a domain with trivial reflections.
+
+    Shown in Oskooi et al. "The failure of perfectly matched layers, and towards their redemption by adiabatic
+    absorbers" Adiabatic layers asymptotically approach an adiabatic limit of zero reflections. This is done by
+    gradually increasing absorption, which results in a truncated domain with only trivial reflections.
+    This implementation assumes that the simulation domain without truncation is a rectangle.
+    """
+
+    def __init__(self, description: Description, round_trip: float = 1e-6, degree: int = 2):
+        self.bbox = BoundingBox2D(
+            0.0, 0.0, description.left_width + description.width + description.right_width, description.height
+        )
+        self.depth = description.absorber_depth
+        self.round_trip = round_trip
+        self.degree = degree
+        self.sigma_0 = -(self.degree + 1) * np.log(round_trip) / (2.0 * self.depth)
+
+    def eval(self, x: np.array) -> np.array:
+        """Returns modification factor to the wave number caused by the adiabatic layer.
+
+        Args:
+            x: location vector
+
+        Returns:
+            zero in areas where the adiabatic is not active, and increasingly complex values inside absorber.
+        """
+        abs_distance = self.bbox.distance(x)
+        rel_distance = abs_distance / self.depth
+
+        return self.sigma_0 * 1j * rel_distance**self.degree
