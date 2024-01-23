9870cc4b8792b6764c1024901e3705edcad5abfa
Jakob Elias Wagner <jakob.e.wagner@tum.de>
2024-01-23 16:10:18 +0100

    fix get mesh; add tests

--- src/nos/data/helmholtz/solver/util.py
+++ src/nos/data/helmholtz/solver/util.py
@@ -8,7 +8,7 @@ from mpi4py import MPI
 
 def get_mesh(
     mesh_file: pathlib.Path, comm: MPI.Comm = MPI.COMM_SELF
-) -> Tuple[dolfinx.mesh.Mesh, dolfinx.mesh.MeshTags, dolfinx.mesh.MeshTags]:
+) -> (Tuple)[dolfinx.mesh.Mesh, dolfinx.mesh.MeshTags, dolfinx.mesh.MeshTags]:
     """Loads mesh from a msh file and returns it as a dolfinx mesh. Initializes mesh on rank 0 of the given
     communicator.
 
@@ -19,5 +19,8 @@ def get_mesh(
     Returns:
         Tuple containing the dolfinx mesh, mesh-tags and boundary tags.
     """
-    gmsh.open(mesh_file)
-    return dolfinx.io.gmshio.model_to_mesh(gmsh.model, comm, 0)
+    gmsh.initialize()
+    gmsh.open(str(mesh_file))
+    mesh, cell_tags, boundary_tags = dolfinx.io.gmshio.model_to_mesh(gmsh.model, comm, 0)
+    gmsh.finalize()
+    return mesh, cell_tags, boundary_tags
