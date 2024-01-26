43a9dca0305f4ab3b31caede83a8a334891815a8
Jakob Elias Wagner <jakob.e.wagner@tum.de>
2024-01-26 11:02:45 +0100

    set gmsh verbosity level to 1 when loading meshes into dolfinx

--- src/nos/data/helmholtz/solver/util.py
+++ src/nos/data/helmholtz/solver/util.py
@@ -20,7 +20,9 @@ def get_mesh(
         Tuple containing the dolfinx mesh, mesh-tags and boundary tags.
     """
     gmsh.initialize()
-    gmsh.open(str(mesh_file))
+    gmsh.option.setNumber("General.Verbosity", 1)
+    gmsh.model.add(f"read form file {mesh_file.stem}")
+    gmsh.merge(str(mesh_file))
     mesh, cell_tags, boundary_tags = dolfinx.io.gmshio.model_to_mesh(gmsh.model, comm, 0)
     gmsh.finalize()
     return mesh, cell_tags, boundary_tags
