14126ee823bd576225c5fdbee7789d2dcf8f04b4
JakobEliasWagner <jakob@winter-wind.de>
2024-03-11 15:33:04 +0100

    modify trainer

--- src/nos/trainer/trainer.py
+++ src/nos/trainer/trainer.py
@@ -1,9 +1,11 @@
 import time
 
 import mlflow
+import torch.optim.lr_scheduler as sched
 import torch.utils.data
 from loguru import logger
 from torch.utils.data import DataLoader, random_split
+from tqdm import tqdm
 
 from continuity.data import OperatorDataset
 from continuity.operators import Operator
@@ -12,11 +14,11 @@ from .average_metric import AverageMetric
 
 
 class Trainer:
-    def __init__(self, criterion, optimizer, scheduler):
+    def __init__(self, criterion, optimizer):
         self.test_val_split = 0.9
         self.criterion = criterion
         self.optimizer = optimizer
-        self.scheduler = scheduler
+        self.scheduler = sched.CosineAnnealingWarmRestarts(optimizer, 100)
 
     def __call__(
         self, operator: Operator, data_set: OperatorDataset, max_epochs: int = 100, batch_size: int = 2**10
@@ -26,16 +28,23 @@ class Trainer:
 
         logger.info(f"Starting training for {max_epochs} epochs on {device}.")
 
-        train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+        train_loader = DataLoader(train_set, batch_size=batch_size)
         val_loader = DataLoader(val_set, batch_size=batch_size)
 
         operator.to(device)
 
         with mlflow.start_run():
-            for epoch in range(max_epochs):
-                logger.info(f"Training progress: {float(epoch) / max_epochs:.0f}%")
-                self.train(train_loader, operator, epoch, device)
-                self.eval(val_loader, operator, epoch, device)
+            pbar = tqdm(range(max_epochs))
+            train_loss = torch.inf
+            val_loss = torch.inf
+            for epoch in pbar:
+                pbar.set_description(
+                    f"Train Loss: {train_loss: .6f},\t Val Loss: {val_loss: .6f}, Lr: {self.optimizer.param_groups[0]['lr']}"
+                )
+                train_loss = self.train(train_loader, operator, epoch, device)
+                val_loss = self.eval(val_loader, operator, epoch, device)
+                self.scheduler.step(epoch)
+                self.log("lr", self.optimizer.param_groups[0]["lr"], epoch)
 
         logger.info("Training finished.")
 
@@ -77,6 +86,7 @@ class Trainer:
         self.log(**data_time.to_dict(), epoch=epoch)
         self.log(**data_transfer.to_dict(), epoch=epoch)
         self.log(**avg_loss.to_dict(), epoch=epoch)
+        return avg_loss.avg
 
     def eval(self, loader, model, epoch, device):
         batch_time = AverageMetric("Eval-time", ":6.3f")
@@ -109,6 +119,7 @@ class Trainer:
         self.log(**data_time.to_dict(), epoch=epoch)
         self.log(**data_transfer.to_dict(), epoch=epoch)
         self.log(**avg_loss.to_dict(), epoch=epoch)
+        return avg_loss.avg
 
     def log(self, name: str, val: float, epoch: int):
         mlflow.log_metric(key=name, value=val, step=epoch)
