339d54d8d7180832aad59c1ff95e4b37d7175544
JakobEliasWagner <jakob@winter-wind.de>
2024-03-19 00:02:12 +0100

    add dno and don

--- src/nos/operators/__init__.py
+++ src/nos/operators/__init__.py
@@ -1,6 +1,12 @@
 from .deep_dot_operator import (
     DeepDotOperator,
 )
+from .deep_neural_operator import (
+    DeepNeuralOperator,
+)
+from .deep_o_net import (
+    DeepONet,
+)
 from .mean_stack_neural_operator import (
     MeanStackNeuralOperator,
 )
@@ -20,6 +26,8 @@ __all__ = [
     "MeanStackNeuralOperator",
     "DeepDotOperator",
     "NeuralOperator",
+    "DeepNeuralOperator",
+    "DeepONet",
     "to_json",
     "to_pt",
     "serialize",
---
+++ src/nos/operators/deep_o_net.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+from continuity.operators import (
+    OperatorShapes,
+)
+
+from nos.networks import (
+    ResNet,
+)
+
+from .operator import (
+    NeuralOperator,
+)
+
+
+class DeepONet(NeuralOperator):
+    def __init__(
+        self,
+        shapes: OperatorShapes,
+        branch_width: int = 32,
+        branch_depth: int = 3,
+        trunk_width: int = 32,
+        trunk_depth: int = 3,
+        stride: int = 1,
+        basis_functions: int = 8,
+        act: nn.Module = None,
+    ):
+        if act is None:
+            act = nn.Tanh()
+        NeuralOperator.__init__(
+            self,
+            shapes=shapes,
+            properties={
+                "branch_width": branch_width,
+                "branch_depth": branch_depth,
+                "trunk_width": trunk_width,
+                "trunk_depth": trunk_depth,
+                "stride": stride,
+                "basis_functions": basis_functions,
+                "act": act.__class__.__name__,
+            },
+        )
+        self.basis_functions = basis_functions
+        self.dot_dim = shapes.v.dim * basis_functions
+        # branch network
+        self.branch_lift = nn.Linear(shapes.u.num * shapes.u.dim, branch_width)
+        self.branch_hidden = ResNet(width=branch_width, depth=branch_depth, act=act, stride=stride)
+        self.branch_project = nn.Linear(branch_width, self.dot_dim)
+        self.branch = nn.Sequential(self.branch_lift, self.branch_hidden, self.branch_project)
+
+        # trunk network
+        self.trunk_lift = nn.Linear(shapes.y.dim, trunk_width)
+        self.trunk_hidden = ResNet(width=trunk_width, depth=trunk_depth, act=act, stride=stride)
+        self.trunk_project = nn.Linear(trunk_width, self.dot_dim)
+        self.trunk = nn.Sequential(self.trunk_lift, self.trunk_hidden, self.trunk_project)
+
+    def forward(self, _: torch.Tensor, u: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        # flatten inputs for both trunk and branch network
+        u = u.flatten(1, -1)
+
+        y = y.flatten(0, 1)
+
+        # Pass through branch and trunk networks
+        b = self.branch(u)
+        t = self.trunk(y)
+
+        # dot product
+        b = b.reshape(-1, self.shapes.v.dim, self.basis_functions)
+        t = t.reshape(
+            b.size(0),
+            -1,
+            self.shapes.v.dim,
+            self.basis_functions,
+        )
+
+        return torch.einsum("abcd,acd->abc", t, b)
