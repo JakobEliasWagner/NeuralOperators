63c3643525563f43c5bb23a63fbc5a430b8ec56b
JakobEliasWagner <jakob@winter-wind.de>
2024-03-22 15:41:11 +0100

    add sine wave parameter transmission loss dataset

--- src/nos/data/__init__.py
+++ src/nos/data/__init__.py
@@ -2,6 +2,7 @@ from .transmission_loss import (
     TLDataset,
     TLDatasetCompact,
     TLDatasetCompactExp,
+    TLDatasetCompactWave,
 )
 
-__all__ = ["TLDataset", "TLDatasetCompact", "TLDatasetCompactExp"]
+__all__ = ["TLDataset", "TLDatasetCompact", "TLDatasetCompactExp", "TLDatasetCompactWave"]
