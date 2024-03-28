d462d13236e2ae7d52f07a68f184facfc076ebef
Jakob <jakob@winter-wind.de>
2024-03-28 19:57:20 +0100

    add plot pressure boundary

--- scripts/train_operator.py
+++ scripts/train_operator.py
@@ -2,41 +2,35 @@ import pathlib
 
 import torch
 import torch.optim.lr_scheduler as sched
-from continuity.discrete import (
-    UniformBoxSampler,
-)
 from loguru import (
     logger,
 )
 
 from nos.data import (
-    IndicatorTLDataset,
+    PressureBoundaryDataset,
 )
 from nos.operators import (
-    DeepOBranchFNO,
+    DeepONet,
 )
 from nos.trainers import (
     Trainer,
 )
 
-BATCH_SIZE = 461
-N_EPOCHS = 1000
-LR = 1e-2
+BATCH_SIZE = 64
+N_EPOCHS = 10000
+LR = 1e-3
 
 
 def main():
-    dataset = IndicatorTLDataset(
-        path=pathlib.Path.cwd().joinpath("data", "train", "transmission_loss_smooth"),
-        sampler=UniformBoxSampler([0.0, 0.0], [22e-3, 22e-3]),
-        n_box_samples=2**10,
-        n_samples=-1,
+    dataset = PressureBoundaryDataset(
+        data_dir=pathlib.Path.cwd().joinpath("data", "train", "pulsating_sphere_450"), n_samples=-1, n_observations=10
     )
 
     logger.info(f"Dataset size: {len(dataset)}")
-    operator = DeepOBranchFNO(dataset.shapes, trunk_width=64, trunk_depth=16, branch_width=8, branch_depth=8)
+    operator = DeepONet(dataset.shapes)
     logger.info("Operator initialized.")
 
-    optimizer = torch.optim.Adam(operator.parameters(), lr=LR, weight_decay=5e-3)
+    optimizer = torch.optim.Adam(operator.parameters(), lr=LR)
     scheduler = sched.CosineAnnealingLR(optimizer, N_EPOCHS)
 
     trainer = Trainer(
---
+++ scripts/visualize_pulsating_sphere.py
@@ -0,0 +1,106 @@
+import pathlib
+
+import matplotlib.pyplot as plt
+import torch
+from continuity.data import (
+    OperatorDataset,
+)
+from continuity.operators import (
+    Operator,
+)
+from torch.utils.data import (
+    DataLoader,
+)
+
+from nos.data import (
+    PressureBoundaryDataset,
+)
+from nos.operators import (
+    deserialize,
+)
+
+plt.rcParams["text.usetex"] = True
+
+
+def plot_three_tile_plot(dataset: OperatorDataset, operator: Operator, out_dir: pathlib.Path):
+    out_dir = out_dir.joinpath("three_tile")
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    plot_loader = DataLoader(dataset, batch_size=1)
+    for i, (x, u, y, v) in enumerate(plot_loader):
+        v_pred = operator(x, u, y)
+
+        x_plot = dataset.transform["x"].undo(x).squeeze().detach()
+        u_plot = dataset.transform["u"].undo(u).squeeze().detach()
+        y_plot = dataset.transform["y"].undo(y).squeeze().detach()
+        v_plot = dataset.transform["v"].undo(v).squeeze().detach()
+        v_pred_plot = dataset.transform["v"].undo(v_pred).squeeze().detach()
+
+        fig, axs = plt.subplots(2, 2, figsize=(12, 12))
+        v = torch.max(torch.abs(u_plot[:, 0]))
+        im = axs[1, 0].tricontourf(
+            x_plot[:, 0], x_plot[:, 1], u_plot[:, 0], levels=256, cmap="seismic", vmin=-v, vmax=v
+        )
+        fig.colorbar(im, ax=axs[1, 0])
+        axs[1, 0].title.set_text("Real pressure field")
+
+        v = torch.max(torch.abs(u_plot[:, 1]))
+        im = axs[0, 1].tricontourf(
+            x_plot[:, 0], x_plot[:, 1], u_plot[:, 1], levels=256, cmap="seismic", vmin=-v, vmax=v
+        )
+        fig.colorbar(im, ax=axs[0, 1])
+        axs[0, 1].title.set_text("Imag pressure field")
+
+        axs[0, 0].plot(y_plot, v_plot[:, 0], "r--", label="$\Re(Y_{gt})(x)$")
+        axs[0, 0].plot(y_plot, v_plot[:, 1], "g--", label="$-\Im(Y_{gt})(x)$")
+        axs[0, 0].plot(y_plot, v_pred_plot[:, 0], "r-", label="$\Re(Y_{pred})(x)$")
+        axs[0, 0].plot(y_plot, v_pred_plot[:, 1], "g-", label="$-\Im(Y_{pred})(x)$")
+        axs[0, 0].set_xlabel("x [m]")
+        axs[0, 0].set_ylabel("Y [1]")
+        axs[0, 0].set_xlim(0, 1)
+        axs[0, 0].set_ylim(0, 1)
+        axs[0, 0].title.set_text("Admittance on top boundary")
+        axs[0, 0].legend()
+
+        axs[1, 1].plot(v_plot[:, 2], y_plot, "r--", label="$\Re(Y_{gt})(y)$")
+        axs[1, 1].plot(v_plot[:, 3], y_plot, "g--", label="$-\Im(Y_{gt})(y)$")
+        axs[1, 1].plot(v_pred_plot[:, 2], y_plot, "r-", label="$\Re(Y_{pred})(y)$")
+        axs[1, 1].plot(v_pred_plot[:, 3], y_plot, "g-", label="$-\Im(Y_{pred})(y)$")
+        axs[1, 1].set_xlabel("Y [1]")
+        axs[1, 1].set_ylabel("y [m]")
+        axs[1, 1].legend()
+        axs[1, 1].set_xlim(0, 1)
+        axs[1, 1].set_ylim(0, 1)
+        axs[1, 1].title.set_text("Admittance on right boundary")
+
+        fig.suptitle("")
+        fig.tight_layout()
+
+        plt_path = out_dir.joinpath(f"plot_{i}.png")
+        plt.savefig(plt_path)
+        plt.close(fig)
+
+
+def main():
+    # load operator
+    operator_dir = pathlib.Path.cwd().joinpath(
+        "run", "2024_03_28_18_38_05-381e718b-50e1-4637-99e2-9965f89e7b69", "DeepONet_2024_03_28_18_57_47"
+    )
+    operator = deserialize(operator_dir)
+
+    # load dataset
+    data_path = pathlib.Path.cwd().joinpath("data", "test", "pulsating_sphere_450")
+    scale_path = pathlib.Path.cwd().joinpath("data", "train", "pulsating_sphere_450")
+    dataset_scale = PressureBoundaryDataset(data_dir=scale_path, n_samples=-1)
+    dataset = PressureBoundaryDataset(data_dir=data_path, n_samples=5)
+    dataset.transform = dataset_scale.transform
+
+    # out dir
+    out_dir = pathlib.Path.cwd().joinpath("out", operator_dir.name)
+
+    # plotting
+    plot_three_tile_plot(dataset, operator, out_dir)
+
+
+if __name__ == "__main__":
+    main()
