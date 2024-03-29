328665dcd3c6a646280c03b4f59012625e519b24
JakobEliasWagner <jakob@winter-wind.de>
2024-03-29 15:48:54 +0100

    fix properties

--- src/nos/operators/belnet.py
+++ src/nos/operators/belnet.py
@@ -24,13 +24,22 @@ class BelNet(BN, NeuralOperator):
         D_1: int = 3,
         N_2: int = 32,
         D_2: int = 3,
-        a_x: Optional[torch.nn.Module] = None,
-        a_u: Optional[torch.nn.Module] = None,
-        a_y: Optional[torch.nn.Module] = None,
+        a_x: Optional[torch.nn.Module] = torch.nn.Tanh(),
+        a_u: Optional[torch.nn.Module] = torch.nn.Tanh(),
+        a_y: Optional[torch.nn.Module] = torch.nn.Tanh(),
     ):
         super().__init__(shapes=shapes, K=K, N_1=N_1, D_1=D_1, N_2=N_2, D_2=D_2, a_x=a_x, a_u=a_u, a_y=a_y)
         NeuralOperator.__init__(
             self,
             shapes=shapes,
-            properties={"K": K, "N_1": N_1, "D_1": D_1, "N_2": N_2, "D_2": D_2, "a_x": a_x, "a_u": a_u, "a_y": a_y},
+            properties={
+                "K": K,
+                "N_1": N_1,
+                "D_1": D_1,
+                "N_2": N_2,
+                "D_2": D_2,
+                "a_x": a_x.__class__.__name__,
+                "a_u": a_u.__class__.__name__,
+                "a_y": a_y.__class__.__name__,
+            },
         )
