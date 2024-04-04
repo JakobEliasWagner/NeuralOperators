7a50fb85f3a80fa75a643ccdd60804158a90cb79
Jakob <jakob@winter-wind.de>
2024-04-04 17:34:47 +0200

    add transforms

--- src/nos/transforms/__init__.py
+++ src/nos/transforms/__init__.py
@@ -1,5 +1,15 @@
-from .scaling import (
+from .log10_scale import (
+    Log10Scale,
+    SymmetricLog10Scale,
+)
+from .min_max_scale import (
     MinMaxScale,
 )
+from .quantile_scaler import (
+    QuantileScaler,
+)
+from .sklearn_preprocess import (
+    SKLearnPreprocess,
+)
 
-__all__ = ["MinMaxScale"]
+__all__ = ["MinMaxScale", "Log10Scale", "SymmetricLog10Scale", "SKLearnPreprocess", "QuantileScaler"]
