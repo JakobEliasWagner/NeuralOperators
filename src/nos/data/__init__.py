c392a07bc99ced57c207a2223244cd0fc00a1307
JakobEliasWagner <42122260+JakobEliasWagner@users.noreply.github.com>
2024-08-19 13:38:06 +0200

    rm wave and exp transmission loss variant (#55)

--- src/nos/data/__init__.py
+++ src/nos/data/__init__.py
@@ -1,13 +1,9 @@
 from .transmission_loss import (
     TLDataset,
     TLDatasetCompact,
-    TLDatasetCompactExp,
-    TLDatasetCompactWave,
 )
 
 __all__ = [
     "TLDataset",
     "TLDatasetCompact",
-    "TLDatasetCompactExp",
-    "TLDatasetCompactWave",
 ]
